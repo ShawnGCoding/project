74dd78fe81078ca98733fbbea91025406a514d6e
xiaoyguan <xiaoyguan@tesla.com>
2023-06-16 12:19:34 +0800

    chore: intergate with scss

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Vue app</div>
+  <div>
+    <h1>zxczxx</h1>
+    <h2>aiuyfhsi</h2>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="scss">
+div {
+  h1 {
+    color: #000fff;
+  }
+  h2 {
+    color: red;
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import "element-plus/dist/index.css";
 //@ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 import gloablComponent from "./index";
+import "@/styles/index.scss";
 
-createApp(App).mount("#app");
-App.use(gloablComponent);
-App.use(ElementPlus, {
+const app = createApp(App);
+app.use(gloablComponent);
+app.use(ElementPlus, {
   locale: zhCn,
 });
+
+app.mount("#app");
