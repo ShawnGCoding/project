b11f4db458bdc71f917fbc515f0c05c371b5863d
xiaoyguan <xiaoyguan@tesla.com>
2023-06-16 11:44:22 +0800

    feature: register SvgIcon

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
+import "virtual:svg-icons-register";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+//@ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import gloablComponent from "./index";
+
 createApp(App).mount("#app");
+App.use(gloablComponent);
 App.use(ElementPlus, {
   locale: zhCn,
 });
